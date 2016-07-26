f6b2dcfa5b549b9eac24827893063cea57e948a9
James Sherry <james.m.sherry@googlemail.com>
2016-07-26 22:46:48 +0100

    corrected firebase call. It works at this point but not through reducer calls

--- js/databases/firebase.js
+++ js/databases/firebase.js
@@ -3,9 +3,11 @@ const config = {
   apiKey: 'AIzaSyCpgo5JuJzGUQpMIloXmHZjQeNPQ_BTqXc',
   authDomain: 'snooker-c85c5.firebaseapp.com',
   databaseURL: 'https://snooker-c85c5.firebaseio.com',
-  storageBucket: '',
+  storageBucket: 'snooker-c85c5.appspot.com',
 };
 
-const fbdb = firebase.initializeApp(config);
+firebase.initializeApp(config);
+
+const fbdb = firebase.database().ref();
 
 export default fbdb;
